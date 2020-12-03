2e7b0b3c2a3c94eb3686043e95832c0b7e08f8da
mngr0 <marco.negrini6@studio.unibo.it>
2020-12-03 14:09:14 +0000

    deployed

--- interfaccia/routine.py
+++ interfaccia/routine.py
@@ -5,10 +5,10 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)  
   
 # GPIO 23 set up as input. It is pulled up to stop false signals  
-#GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
 GPIO.setup(24, GPIO.OUT)
 
-GPIO.output(24, 1)
+GPIO.output(24, 0)
 
 
 from interfaccia.models import Configurazione
@@ -20,12 +20,10 @@ def thread_function():
     print("Thread  starting")
     while 1:
         print("thread running at ",Configurazione.objects.get(pk=1).durata)
-        #GPIO.wait_for_edge(23, GPIO.FALLING) 
+        GPIO.wait_for_edge(23, GPIO.FALLING) 
         GPIO.output(24, 1)
         time.sleep(int(Configurazione.objects.get(pk=1).durata)/1000)
         GPIO.output(24, 0)
-        time.sleep(int(Configurazione.objects.get(pk=1).durata)/1000)
-        GPIO.output(24, 1)
 
 
 def set_interval(idx,new_interval):
